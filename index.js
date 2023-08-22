0ab22e03ce1ea46a021ef0d050fae44cef66fa42
RubanRavikumar <rruban006@gmail.com>
2023-08-22 12:57:35 +0530

    Added back end coad

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const fs = require("fs");
-const content = new Date().toString().replace(/[:.]/g,'-');
-// const path = `./files/${content}.txt`;
-const path = "./files/test.txt";
+// run `node index.js` in the terminal
+// STEP 1: IMPORT ALL NECESSARY PACKAGES
+const express = require("express");
+const HTTP_SERVER = express();
+const cors = require("cors");
+var bodyParser = require('body-parser')
 
-try {
-  // file written successfully
-  fs.writeFileSync(path, content);
-  // Read the same file
-  fs.readFile(path, "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
-  });
-} catch (err) {
-  console.error(err);
-}
+HTTP_SERVER.use(cors());
+
+// parse application/x-www-form-urlencoded
+HTTP_SERVER.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+HTTP_SERVER.use(bodyParser.json())
+
+// BASIC SERVER CONFIGS
+const PORT = 5000;
+
+HTTP_SERVER.listen(PORT, "0.0.0.0", (err) => {
+  if (err) throw err;
+  console.log(`SERVER STARTED IN THE PORT ${PORT}`);
+});
+
+// INJECTING API SERVER
+HTTP_SERVER.use("/", require("./app"));
